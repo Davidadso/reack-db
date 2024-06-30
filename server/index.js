ef7e3e00e48ff864c912e601324b21122e24491f
Jesus David <jesusalondon2016@gmail.com>
2024-06-30 16:07:53 -0500

    nuevamente

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { registrarUsuario, iniciarSesion } = require('./Controller/userController');
+const { registrarUsuario, iniciarSesion } = require('./Controllers/userController');
 
 const app = express();
 
@@ -12,10 +12,15 @@ app.use(express.json());
 app.post('/api/usuarios/registro', registrarUsuario);
 app.post('/api/usuarios/login', iniciarSesion);
 
+// Manejo de rutas no encontradas
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
 // Manejo de errores global
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Algo salió mal!');
+    res.status(500).json({ error: 'Algo salió mal', details: err.message });
 });
 
 // Iniciar el servidor
