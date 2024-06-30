190da754756efec1acd3d16766126567cacb6beb
Jesus David <jesusalondon2016@gmail.com>
2024-06-30 15:53:21 -0500

    organizando

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { registrarUsuario, iniciarSesion } = require('./controladores/usuarioController');
+const { registrarUsuario, iniciarSesion } = require('./Controller/usuarioController');
 
 const app = express();
 
